4226a1f80d1f7b5e9d31120a912ea15c4188f634
DeverLoper513 <151453624+DeverLoper513@users.noreply.github.com>
2025-01-18 00:41:50 +0800

    Update ScriptClip.jsx

--- src/clipboards/ScriptClip.jsx
+++ src/clipboards/ScriptClip.jsx
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ScriptClipBoard() {
   const [value, setValue] = useState(`
-  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/VdV66yJVuakxPMme4IZourUrTov3R5KO6wtqlDI2" async></script>
+  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/pj2zhuTwcWuL8aUXdRktBsp1uZ1uWQixBRV7CCMw" async></script>
     `);
   const [isCopied, setCopied] = useState(false);
 
